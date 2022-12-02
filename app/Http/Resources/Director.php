a4ef14bc18139b2bcece0aad61cd43893dc4db80
Alumnado Mañana <joseivansantanaperez@alumno.ieselrincon.es>
2022-12-02 19:47:01 +0000

    third commit

--- app/Http/Resources/Director.php
+++ app/Http/Resources/Director.php
@@ -4,14 +4,13 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Blog extends JsonResource
+class Director extends JsonResource
 {
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'title' => $this->title,
-            'description' => $this->description,
+            'apodo' => $this->apodo,
             'created_at' => $this->created_at->format('m/d/Y'),
             'updated_at' => $this->updated_at->format('m/d/Y')
         ];
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
   
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BlogController;
+use App\Http\Controllers\API\EventController;
+use App\Http\Controllers\API\DirectorController;
   
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +19,6 @@ Route::post('register', [AuthController::class, 'signup']);
      
 Route::middleware('auth:sanctum')->group( function () {
     Route::resource('blogs', BlogController::class);
+    Route::resource('events', EventController::class);
+    Route::resource('directors', DirectorController::class);
 });
