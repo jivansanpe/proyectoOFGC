80674b1939fd295740cf85ddb1613e7d1ea62671
Ivan <joseivansantanaperez@alumno.ieselrincon.es>
2022-12-19 07:36:41 +0000

    alert confirm log-out

--- frontendEste/src/app/menu/menu.component.ts
+++ frontendEste/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -7,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  logOut() {
+    if (confirm('¿Está seguro de que desea cerrar sesión?')) {
+      this.router.navigate(['/login']);
+    }
+  }
 
 }
