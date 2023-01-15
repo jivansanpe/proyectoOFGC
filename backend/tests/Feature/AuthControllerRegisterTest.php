4112ac4f980c6a8325eb37ef8d0b04494d4dff54
José Iván Santana Pérez <joseivansantanaperez@alumno.ieselrincon.es>
2023-01-15 03:02:29 +0000

    2 Test en el backend

---
+++ backend/tests/Feature/AuthControllerRegisterTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\User;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use WithoutMiddleware;
+    protected static $registerData = [];
+    public function testRegister()
+    {
+        self::$registerData = [
+            'name' => $randomString = bin2hex(random_bytes(5)),
+            'email' => $randomString.'@example.com',
+            'password' => 'password',
+            'confirm_password' => 'password',
+        ];
+
+        $response = $this->json('POST', '/api/register', self::$registerData);
+
+        $response->assertStatus(200)->assertJson([
+            'message' => 'User created successfully.'
+        ]);
+    }
+    public function testLogin()
+    {
+        $loginData = [
+            'email' => self::$registerData['email'],
+            'password' => self::$registerData['password'],
+        ];
+        $response = $this->json('POST', '/api/login', $loginData);
+
+        // Assert that the login was successful
+        $response->assertStatus(200)->assertJson([
+            'message' => 'User signed in'
+        ]);
+    }
+}
